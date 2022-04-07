da7f5dc69822279830ccf01bdc3f61c00dcaac89
Marc <marcortizgonzalez2002@gmail.com>
2022-04-07 19:18:16 +0200

    Login,Registro,Footer

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <link rel="stylesheet" href="{!! asset ('css/registro/loginregistro.css')!!}">
+    <script src="{!! asset ('js/fontawesomePRO.js')!!}"></script>
+    <link href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" rel="stylesheet">
+    <title>Login page</title>
 </head>
-<body>
-    <form action="{{url('login')}}" method="post">
-        @csrf
-        <label for="">Correo/Nick</label>
-        <input type="text" name="correo_nick" id="">
-        <label for="">Contraseña</label>
-        <input type="password" name="contra_usu" id="">
-        <input type="submit" value="Enviar">
-    </form>
+
+<body class="login-page">
+    <header>
+    </header>
+    <main>
+        <div class="region region1">
+            <div class="content-region">
+                <div class="glassland">
+                    <div class="content-glassland">
+                        <div class="login-content-glassland">
+                            <img src="{!! asset ('media/3Dicons-dinamicos/icons/png/boy\boy-dynamic-gradient.png') !!}" width="80px" height="80px">
+                            <h2>¡Inicia sesión y disfruta de NoteHub!</h2>
+                            <br>
+                            <form action="{{url('login')}}" method="post">
+                                @csrf
+                                <div>
+                                    <p class="pizq">Email o Nickname</p>
+                                    <input class="inputbtn" type="email" name="correo_nick" id="correo_nick" placeholder="&#xf007">
+                                </div>
+                                <div>
+                                    <p class="pizq">Contraseña</p>
+                                    <input class="inputbtn" type="password" name="contra_usu" id="contra_usu" placeholder="&#xf023">
+                                </div>
+                                <!-- <div class="alienarright">
+                                    <a href="">
+                                        <p>Contraseña olvidada?</p>
+                                    </a>
+                                </div> -->
+                                <input class="login-btn-absglass" type="submit" value="ENTRAR"></input>
+                                <div class="">
+                                    <div class="alienarleft">
+                                        <p>No estás registrado?</p>
+                                    </div>
+                                    <div class="alienarleft">
+                                        <a href="{{url('register')}}">
+                                            <p>Crear cuenta</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-region">
+                <div class="imagenrelleno">
+                    <img src="{!! asset ('media/fotos_loginregister/imagen1.png') !!}" alt="imglogin" class="imgredondeada">
+                </div>
+            </div>
+        </div>
+    </main>
+
+    {{-- <footer class="footer-distributed">
+
+        <div class="footer-right">
+
+            <a href="https://www.facebook.com/notehub.oficial"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://twitter.com/notehub_oficial"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.instagram.com/notehub.oficial"><i class="fa-brands fa-instagram"></i></a>
+
+        </div>
+
+        <div class="footer-left">
+
+            <p class="footer-links">
+
+                <a class="link-1">2022</a>
+
+                <a href="https://goo.gl/maps/TVrDESsEywPBnykd6">Hospitalet de Llobregat</a>
+
+                <a href="mailto:notehubdaw2@gmail.com">notehubdaw2@gmail.com</a>
+            </p>
+
+            <p>NoteHub &copy; 2022</p>
+        </div>
+
+    </footer> --}}
 </body>
 </html>
+@include ('template.footer')
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,75 +5,166 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="../public/js/registro/registro.js"></script>
-    <link rel="stylesheet" href="../public/css/registro/registro.css">
+    <link rel="stylesheet" href="{!! asset ('css/registro/loginregistro.css')!!}">
+    <script src="{!! asset ('js/fontawesomePRO.js')!!}"></script>
     <title>Register</title>
 </head>
-<body>
-        @if($errors->any())
-            @error('nombre_usu')
-            <p>{{$message}}</p>
-            @enderror
-            @error('apellido_usu')
-            <p>{{$message}}</p>
-            @enderror
-            @error('nick_usu')
-            <p>{{$message}}</p>
-            @enderror
-            @error('fecha_nac_usu')
-            <p>{{$message}}</p>
-            @enderror
-            @error('centro')
-            <p>{{$message}}</p>
-            @enderror
-            @error('correo_usu')
-            <p>{{$message}}</p>
-            @enderror
-            @error('contra_usu')
-            <p>{{$message}}</p>
-            @enderror
-            @error('contra_usu_verify')
-            <p>{{$message}}</p>
-            @enderror
-        @endif  
-
-    <form action="{{url('register')}}" onsubmit="return hasAvatarOrImage();" method="post" enctype="multipart/form-data">
-        @csrf
-        <label for="">Nombre</label>
-        <input type="text" name="nombre_usu" id="">
-        <label for="">Apellido</label>
-        <input type="text" name="apellido_usu" id="">
-        <label for="">Nickname</label>
-        <input type="text" name="nick_usu" id="">
-        <label for="">Fecha de nacimiento</label>
-        <input type="date" name="fecha_nac_usu" id="">
-        <label for="">Centro</label>
-            <select name="centro" id="">
-                <option value="">--</option>
-                @foreach($centros as $centro)
-                    <option value="{{$centro->id}}">{{$centro->nombre_centro}}</option>
-                @endforeach
-            </select>
-        <label for="">Correo</label>
-        <input type="text" name="correo_usu" id="">
-        <label for="">Contraseña</label>
-        <input type="password" name="contra_usu" id="">
-        <label for="">Verificar contraseña</label>
-        <input type="password" name="contra_usu_verify" id="">
-        <div id="myModal" class="modal">
-            <div class="modal-content">
-                <h1>Escoge tu avatar</h1>
-                <span class="close" onclick="closeModal();">&times;</span>
-                @foreach($avatares as $avatar)
-                    <button onclick="closeModal();avatarSelected('{{$avatar->img_avatar}}');return false;"><img src={{asset('storage').'/'.$avatar->img_avatar}} width="100px" height="100px"></button>
-                    <p>{{$avatar->img_avatar}}</p>
-                @endforeach
-                <input type="file" name="img_avatar_usu" id="img_avatar_usu">
+<body class="registro-page">
+    <header>
+    </header>
+    <main> 
+        <div class="region region1">
+            <div class="content-region">
+                <div class="glassland">
+                    <div class="content-glassland">
+                        <div class="registro-content-glassland">
+                            <img src="{!! asset ('media/3Dicons-dinamicos/icons/png/boy\boy-dynamic-gradient.png') !!}" width="80px" height="80px">
+                            <h2>¡Regístrate y accede a todas las ventajas de NoteHub!</h2>
+                            <br>
+                            <form action="{{url('register')}}" onsubmit="return hasAvatarOrImage();" method="post" enctype="multipart/form-data">
+                                @csrf
+                                <div class="menudos">
+                                    <p class="pizq">Nombre</p>
+                                    <input class="inputbtn" type="text" name="nombre_usu" id="nombre_usu" placeholder="Nombre">
+                                    @error('nombre_usu')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="menudos">
+                                    <p class="pizq">Apellido</p>
+                                    <input class="inputbtn" type="text" name="apellido_usu" id="apellido_usu" placeholder="Apellido">
+                                    @error('apellido_usu')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="menudos">
+                                    <p class="pizq">Nickname</p>
+                                    <input class="inputbtn" type="text" name="nick_usu" id="nick_usu" placeholder="Nickname">
+                                    @error('nick_usu')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="menudos">
+                                    <p class="pizq">Fecha de nacimiento</p>
+                                    <input class="inputbtn" type="date" name="fecha_nac_usu" id="fecha_nac_usu">
+                                    @error('fecha_nac_usu')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="menudos">
+                                    <p class="pizq">Centro</p>
+                                    <select class="inputbtn" name="centro" id="centro">
+                                        <option value="">--</option>
+                                        @foreach($centros as $centro)
+                                            <option value="{{$centro->id}}">{{$centro->nombre_centro}}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('centro')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="menudos">
+                                    <p class="pizq">Email</p>
+                                    <input class="inputbtn" type="email" name="correo_usu" id="correo_usu" placeholder="Email">
+                                    @error('correo_usu')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="menudos">
+                                    <p class="pizq">Contraseña</p>
+                                    <input class="inputbtn" type="password" name="contra_usu" id="contra_usu" placeholder="Contraseña">
+                                    @error('contra_usu')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div class="menudos">
+                                    <p class="pizq">Repetir contraseña</p>
+                                    <input class="inputbtn" type="password" name="contra_usu_verify" id="contra_usu_verify" placeholder="Repetir contraseña">
+                                    @error('contra_usu_verify')
+                                    <p>{{$message}}</p>
+                                    @enderror
+                                </div>
+                                <div id="myModal" class="modal">
+                                    <div class="modal-content">
+                                        <span class="close" onclick="closeModal();">&times;</span>
+                                        <br><br>
+                                        <h1>Escoge tu avatar</h1>
+                                        <br>
+                                        <p style="float: left; padding-left:75px">Selecciona un avatar predefinido:</p>
+                                        <br>
+                                        <div class="grid">
+                                            @foreach($avatares as $avatar)
+                                            <div class="modal-3">
+                                                <button onclick="avatarSelected('{{$avatar->img_avatar}}');return false;"><img src={{asset('storage').'/'.$avatar->img_avatar}} width="100px" height="100px"></button>
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                        <p style="float: left; padding-left:75px">o sube tu propio avatar:</p>
+                                        <input type="file" name="img_avatar_usu" id="img_avatar_usu">
+                                        <br><br>
+                                        <button onclick="closeModal();return false;" class="aceptarbtn" value="Aceptar">Aceptar</button>
+                                    </div>
+                                </div>
+                                <div class="menuuno">
+                                    <p class="pizq">Avatar</p>
+                                    <input class="inputbtn-selec" type="submit" onclick="modalbox();return false;" value="CLICK PARA SELECCIONAR AVATAR">
+                                    <input type="hidden" name="img_avatar_sistema" id="img_avatar_sistema" value="">
+                                </div>
+                                <!-- <div class="alienarright">
+                                    <a href="">
+                                        <p>Contraseña olvidada?</p>
+                                    </a>
+                                </div> -->
+                                <div class="menuuno">
+                                    <input class="login-btn-absglass" type="submit" value="ENTRAR"></input>
+                                </div>
+                                <div class="">
+                                    <div class="alienarleft">
+                                        <p>Ya estás registrado?</p>
+                                    </div>
+                                    <div class="alienarleft">
+                                        <a href="{{url('login')}}">
+                                            <p>Inicia sesión</p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-region">
+                <div class="imagenrelleno">
+                    <img src="{!! asset ('media/fotos_loginregister/imagen2.2.png') !!}" alt="imgregistro" class="imgredondeada2">
+                </div>
             </div>
         </div>
-        <label for="">Selecciona tu avatar</label>
-        <button type="submit" value="avatar" onclick="modalbox();return false;">Avatar</button>
-        <input type="hidden" name="img_avatar_sistema" id="img_avatar_sistema" value="">
-        <input type="submit" value="Enviar">
-    </form>
+    </main>
+    {{-- <footer class="footer-distributed">
+
+        <div class="footer-right">
+
+            <a href="https://www.facebook.com/notehub.oficial"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://twitter.com/notehub_oficial"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.instagram.com/notehub.oficial"><i class="fa-brands fa-instagram"></i></a>
+
+        </div>
+
+        <div class="footer-left">
+
+            <p class="footer-links">
+
+                <a class="link-1">2022</a>
+
+                <a href="https://goo.gl/maps/TVrDESsEywPBnykd6">Hospitalet de Llobregat</a>
+
+                <a href="mailto:notehubdaw2@gmail.com">notehubdaw2@gmail.com</a>
+            </p>
+
+            <p>NoteHub &copy; 2022</p>
+        </div>
+
+    </footer> --}}
 </body>
 </html>
+@include ('template.footer')
