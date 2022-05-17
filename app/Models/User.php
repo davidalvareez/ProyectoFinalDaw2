248d081409e6413010beb3ff9eaa6035b3df61ba
Xavi <100006207.joan23@fje.edu>
2022-05-17 18:27:42 +0200

    Intento de chat

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table = "tbl_usuarios_test";
     protected $fillable = [
         'name',
         'email',
