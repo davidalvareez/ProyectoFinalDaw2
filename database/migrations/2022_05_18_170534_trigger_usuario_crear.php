1e25e5c71c926735a689d83401931da0c00a84fc
Xavi <100006207.joan23@fje.edu>
2022-05-19 16:26:45 +0200

    Chat completado junto con trigger

--- database/migrations/2022_05_18_170534_trigger_usuario_crear.php
+++ database/migrations/2022_05_18_170534_trigger_usuario_crear.php
@@ -25,11 +25,11 @@ class TriggerUsuarioCrear extends Migration
           SET @uuid = uuid();
           SET @token = NULL;
           SET @createdAT = NULL;
-          SET @updatedAT =  NULL; 
-          IF (NEW.id_usu != NULL) THEN BEGIN 
+          SET @updatedAT =  NULL;
+          SET @idUsu = NEW.id_usu; 
+          IF NEW.id_usu IS NOT NULL THEN 
                     INSERT INTO users (`name`,`email`,`email_verified_at`,`password`,`uuid`,`image`,`remember_token`,`created_at`,`updated_at`)
                     VALUES (@nombre, @email, @emailVerified, @password, @uuid, @avatarName, @token, @createdAT, @updatedAT);
-            END;
           END IF;
         END;");
     }
